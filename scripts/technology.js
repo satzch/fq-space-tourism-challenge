e9a8d1d4f084633bd5fbc9159daa0af9d0a6d15f
satyam karmakar <gamingsplay240@gmail.com>
2022-10-14 00:15:53 +0530

    add technology page and some css change for compatibility issue

---
+++ scripts/technology.js
@@ -0,0 +1,44 @@
+const technologyImage = document.querySelector(".technology-image")
+const technologyName = document.querySelector(".technology-heading")
+const technologyDesc = document.querySelector(".technology-desc")
+const technology = document.querySelector(".technology-subheading")
+const circles = document.querySelector(".technology-list-circles")
+
+// console.log(circles.children)
+
+let prevOption, option = 0
+
+circles.addEventListener("click", (e) => {
+    e.preventDefault()
+    prevOption = option
+    option = e.target.getAttribute("value") ? e.target.getAttribute("value") : prevOption
+    if (e.target.classList.contains("circle")) {
+        for (let circle of circles.children) {
+            // console.log(circle);
+            circle.firstElementChild.classList.remove("active-circle")
+        }
+        e.target.classList.add("active-circle")
+    }
+
+    // console.log(e.target);
+    // console.log(e.target.getAttribute("value"));
+    updateInfo(option)
+})
+
+let promiseData = fetch("https://raw.githubusercontent.com/satzch/fq-space-tourism-challenge/main/data.json")
+    .then(response => response.json())
+    .then(json => { return json })
+
+
+const updateInfo = async (option) => {
+    const data = await promiseData
+    // console.log(data);
+
+    technologyImage.src = data.technology[option].images.landscape
+    technologyName.innerText = data.technology[option].name
+    technologyDesc.innerText = data.technology[option].description
+
+    // console.log(data.crew[option])
+};
+
+updateInfo(option)
